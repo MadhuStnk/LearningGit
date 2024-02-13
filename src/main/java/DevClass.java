745c03d02f41b76de4d7b5f03303a9da7ec4fbf8
MadhuStnk <159882599+MadhuStnk@users.noreply.github.com>
2024-02-13 13:47:27 +0530

    Updated
    Updated

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass+hello");
+        System.out.println("DevClass+hello-hiii");
         //Added
     }
 }
